ffd929d55214a111a501dfa70395a1696c70ae75
viswa262006 <viswa262006@gmail.com>
2025-12-04 01:26:22 -0800

    Add files via upload
    activity1

---
+++ activity1.java
@@ -0,0 +1,14 @@
+package au6omation;
+
+public class activity1 {
+
+	public static void main(String[] args) {
+		int age=19;
+				char logo='K';
+				String name="viswanaathan";
+				boolean value=true;
+				System.out.println(age);
+
+	}
+
+}
