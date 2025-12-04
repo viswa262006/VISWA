3466cb2228c5a3fadaa81bc494e00ae5af9d1938
viswa262006 <viswa262006@gmail.com>
2025-12-04 02:03:47 -0800

    Add files via upload

---
+++ LEARNLOOPS.java
@@ -0,0 +1,13 @@
+package au6omation;
+
+public class LEARNLOOPS {
+
+	public static void main(String[] args) {
+		//1  2  3  4  5
+		for (int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
