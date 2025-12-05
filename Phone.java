ba14f187029eab001d2dc35ccbb8dfbe2a364f48
viswa262006 <viswa262006@gmail.com>
2025-12-04 21:25:11 -0800

    Add files via upload
    phone

---
+++ Phone.java
@@ -0,0 +1,18 @@
+package au6omation;
+
+		public class Phone {
+	
+		
+		//methods  -  action can be performed in mobile
+		//takePHOTO,MAKEcall
+		//CLASSname objectNAME=classNAME();
+		public void takephoto() {
+			System.out.println("Take photo");
+		}
+		
+		public static void main (String[] arg) {
+			Phone obj=new Phone();
+			obj.takephoto();
+	}
+
+}
